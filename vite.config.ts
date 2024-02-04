8660525e1bac197648ea3b5fa298afaf65928ed3
Shivesh Shandilya <shivesh.shandilya@gmail.com>
2024-02-05 03:40:20 +0530

    Deploy changes for github

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import postcss from 'vite-plugin-postcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/statistically/', // Replace with your actual repository name
   plugins: [react()],
 });
